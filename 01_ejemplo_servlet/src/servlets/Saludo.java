1dae6c2a94d5c4df47c03573fdffb91627459245
cris <cris@contactweb.com>
2021-02-19 00:18:26 +0100

    se agrega serialVersionUID

--- 01_ejemplo_servlet/src/servlets/Saludo.java
+++ 01_ejemplo_servlet/src/servlets/Saludo.java
@@ -14,11 +14,8 @@ import javax.servlet.http.HttpServletResponse;
  */
 @WebServlet("/Saludo")
 public class Saludo extends HttpServlet {
-	//private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = 1L;
 
-	/**
-	 * @see HttpServlet#service(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		response.setContentType("text/html");
 		PrintWriter out = response.getWriter();
